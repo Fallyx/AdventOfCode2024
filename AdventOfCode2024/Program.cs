8a42ec60d4ac61db0913fea295cb0d32f69a6dd2
Fallyx <Fallyx@users.noreply.github.com>
2024-11-29 20:14:46 +0100

    Preparation for AoC

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,44 +1,42 @@
 ﻿using System.Diagnostics;
 
-namespace AdventOfCode2024
+namespace AdventOfCode2024;
+internal class Program
 {
-    internal class Program
+    private static readonly Stopwatch stopwatch = new();
+
+    static void Main(string[] args)
     {
-        private static readonly Stopwatch stopwatch = new();
-
-        static void Main(string[] args)
+        string? input;
+        if (args.Length != 0)
         {
-            string? input;
-            if (args.Length != 0)
-            {
-                input = args[0];
-            }
-            else
-            {
-                Console.WriteLine("Run a day [1-25]:");
-                input = Console.ReadLine();
-                Console.WriteLine("-----------------");
-            }
-
-            bool success = int.TryParse(input, out int day);
-            if (!success) return;
-
-            RunDay(day);
+            input = args[0];
+        }
+        else
+        {
+            Console.WriteLine("Run a day [1-25]:");
+            input = Console.ReadLine();
+            Console.WriteLine("-----------------");
         }
 
-        private static void RunDay(int day)
+        bool success = int.TryParse(input, out int day);
+        if (!success) return;
+
+        RunDay(day);
+    }
+
+    private static void RunDay(int day)
+    {
+        stopwatch.Start();
+
+        switch(day)
         {
-            stopwatch.Start();
-
-            switch(day)
-            {
-                case 1:
-
-                    break;
-            }
-
-            stopwatch.Stop();
-            Console.WriteLine($"Day {day:D2} finished in {stopwatch.Elapsed}");
+            case 1:
+                Day01.Day01.Task1();
+                break;
         }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Day {day:D2} finished in {stopwatch.Elapsed}");
     }
 }
