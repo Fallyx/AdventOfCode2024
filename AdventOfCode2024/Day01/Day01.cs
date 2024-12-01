e555f4d5563b1a1c7ffe23cf3ccf9352e6a642b7
Fallyx <Fallyx@users.noreply.github.com>
2024-12-01 20:42:20 +0100

    Day 1 Task 1 and 2

--- AdventOfCode2024/Day01/Day01.cs
+++ AdventOfCode2024/Day01/Day01.cs
@@ -1,11 +1,42 @@
-﻿namespace AdventOfCode2024.Day01;
+﻿using System.Text.RegularExpressions;
 
-internal class Day01
+namespace AdventOfCode2024.Day01;
+
+internal partial class Day01
 {
     const string inputPath = @"Day01/Input.txt";
+    [GeneratedRegex(@"(\d+)\s+(\d+)")]
+    private static partial Regex numberRegex();
 
     internal static void Task1()
     {
+        List<string> lines = [.. File.ReadAllLines(inputPath)];
+        List<int> left = new(lines.Count);
+        List<int> right = new(lines.Count);
+        
+        foreach(string line in lines)
+        {
+            Match match = numberRegex().Match(line);
+            if (match.Success)
+            {
+                left.Add(int.Parse(match.Groups[1].ToString()));
+                right.Add(int.Parse(match.Groups[2].ToString()));
+            }
+        }
 
+        left.Sort();
+        right.Sort();
+
+        int totDistance = 0;
+        int simScore = 0;
+
+        for(int i = 0; i < left.Count; i++)
+        {
+            totDistance += Math.Abs(left[i] - right[i]);
+            simScore += left[i] * right.Where(num => num == left[i]).Count();
+        }
+
+        Console.WriteLine($"Task 1: {totDistance}");
+        Console.WriteLine($"Task 2: {simScore}");
     }
 }
