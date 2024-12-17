4cf556b860c9b0dfc490443c48fc21a0c4f16980
Fallyx <Fallyx@users.noreply.github.com>
2024-12-17 20:43:41 +0100

    Day 17 Part 2

--- AdventOfCode2024/Day17/Day17.cs
+++ AdventOfCode2024/Day17/Day17.cs
@@ -10,8 +10,7 @@ internal class Day17
         long b = 0;
         long c = 0;
 
-        int[] program = null;
-        List<int> outputs = [];
+        List<int> program = [];
 
         foreach (string line in File.ReadLines(inputPath))
         {
@@ -22,16 +21,27 @@ internal class Day17
             else if (line.StartsWith("Register C:"))
                 c = long.Parse(line.Split(' ')[2]);
             else if (line.StartsWith("Program:"))
-                program = line.Split([':', ' ', ','], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToArray();
+                program = line.Split([':', ' ', ','], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
         }
 
+        List<int> outputs = RunProgram(a, b, c, program);
+        Console.WriteLine($"Task 1: {string.Join(",", outputs)}");
+
+        long minA = RunProgram(program);
+        Console.WriteLine($"Task 2: {minA}");
+    }
+
+    private static List<int> RunProgram(long a, long b, long c, List<int> program)
+    {
+        List<int> outputs = [];
         int idx = 0;
-        while(idx < program.Length)
+
+        while (idx < program.Count)
         {
             int opCode = program[idx];
             int operand = program[idx + 1];
-
             long comboOperand = GetComboOperand(operand, a, b, c);
+
             if (opCode == 0)
                 a /= (long)Math.Pow(2, comboOperand);
             else if (opCode == 1)
@@ -55,63 +65,48 @@ internal class Day17
             idx += 2;
         }
 
-        Console.WriteLine($"Task 1: {string.Join(",", outputs)}");        
+        return outputs;
+    }
 
-        for (long a2 = 0; a2 < long.MaxValue; a2++)
+    private static long RunProgram(List<int> program)
+    {
+        Queue<long> queue = new([1, 2, 3, 4, 5, 6, 7, 8]);
+        long minA = long.MaxValue;
+
+        while (queue.Count > 0)
         {
-            idx = 0;
-            a = a2;
-            b = 0;
-            c = 0;
-            outputs = [];
-            while (idx < program.Length)
+            long a = queue.Dequeue();
+            long b = 0;
+            long c = 0;
+
+            List<int> sameOutputs = sameOutputs = RunProgram(a, b, c, program);
+
+            if (sameOutputs.Count > program.Count)
+                continue;
+
+            bool isSubset = true;
+            for (int x = 0; x < sameOutputs.Count; x++)
             {
-                int opCode = program[idx];
-                int operand = program[idx + 1];
-
-                long comboOperand = GetComboOperand(operand, a, b, c);
-                if (opCode == 0)
-                    a /= (long)Math.Pow(2, comboOperand);
-                else if (opCode == 1)
-                    b ^= operand;
-                else if (opCode == 2)
-                    b = comboOperand % 8;
-                else if (opCode == 3 && a != 0)
+                if (sameOutputs[sameOutputs.Count - 1 - x] != program[program.Count - 1 - x])
                 {
-                    idx = operand;
-                    idx -= 2;
-                }
-                else if (opCode == 4)
-                    b ^= c;
-                else if (opCode == 5)
-                    outputs.Add((int)(comboOperand % 8));
-                else if (opCode == 6)
-                    b = a / (long)Math.Pow(2, comboOperand);
-                else if (opCode == 7)
-                    c = a / (long)Math.Pow(2, comboOperand);
-
-                idx += 2;
-            }
-
-            if (outputs.Count == program.Length)
-            {
-                bool found = true;
-                for (int i = 0; i < program.Length; i++)
-                {
-                    if (program[i] != outputs[i])
-                    {
-                        found = false;
-                        break;
-                    }
-
-                }
-                if (found)
-                {
-                    Console.WriteLine($"Task 2: {a2}");
+                    isSubset = false;
                     break;
                 }
             }
+
+            if (isSubset && sameOutputs.Count < program.Count)
+            {
+                a *= 8;
+                for (int i = 0; i < 8; i++)
+                {
+                    queue.Enqueue(a + i);
+                }
+            }
+            else if (isSubset && sameOutputs.Count == program.Count && minA > a)
+                    minA = a;
         }
+
+        return minA;
     }
 
     private static long GetComboOperand(int comboOperand, long a, long b, long c) => comboOperand switch
@@ -122,5 +117,4 @@ internal class Day17
         6 => c,
         7 => throw new Exception("ERROR")
     };
-
 }
